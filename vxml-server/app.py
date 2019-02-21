15f23b1eed26b105e2a5891790a239103dff294f
eliyetres <eliyetres@hotmail.com>
2019-02-21 10:59:32 +0000

    forgot asking for leave date, updated part B

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 temp = '13'
 
+
 @app.route('/weather')
 def hello():
     vxml = render_template('weather.xml', temp=temp)
@@ -10,23 +11,10 @@ def hello():
     response.headers["Content-Type"] = "application/xml"
     return response
 
+
 @app.route('/lab1')
 def lab1():
     vxml = render_template('lab1.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
-
-@app.route('/lab1_menu')
-def lab1_menu():
-    vxml = render_template('lab1_menu.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
-
-@app.route('/lab1_delayed')
-def lab1_delayed():
-    vxml = render_template('lab1_delayed.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
@@ -38,12 +26,28 @@ def lab1_booking():
     response.headers["Content-Type"] = "application/xml"
     return response
 
+@app.route('/lab1_delayed')
+def lab1_delayed():
+    vxml = render_template('lab1_delayed.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route('/lab1_menu')
+def lab1_menu():
+    vxml = render_template('lab1_menu.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+
 @app.route('/lab2')
 def lab2():
     vxml = render_template('lab2.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
+
 
 @app.route('/grammars/<path:path>')
 def send_grammar(path):
